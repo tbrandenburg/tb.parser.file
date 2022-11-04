5bde1b14555604f95d6fe873a6846f904526b434
Tom Brandenburg <tom.brandenburg@gmx.de>
2022-11-04 19:32:20 +0100

    added str and todict methods

--- file_parser.py
+++ file_parser.py
@@ -6,7 +6,7 @@ import pprint
 class File:
     isDirectory: bool
     
-    def __init__(self,name,isDirectory):
+    def __init__(self, name, isDirectory):
       self.name  = name
       self.isDirectory = isDirectory
       self.children = list()
@@ -14,13 +14,13 @@ class File:
     def add_child(self,child):
         self.children.append(child)
 
-    def add_children(self,children):
+    def add_children(self, children):
         self.children.extend(children)
 
 class FileTree:
     fileTree: list
 
-    def __parse_file_tree(self,path):
+    def __parse_file_tree(self, path):
         fileList = list()
       
         for dirEntry in os.scandir(path):
@@ -32,37 +32,54 @@ class FileTree:
                 fileList.append(File(dirEntry.name,False))
         return fileList
     
-    def __init__(self,root):
+    def __init__(self, root):
       self.fileTree = self.__parse_file_tree(root)
 
     def get_file_tree(self):
         return self.fileTree
 
-    def __print_file_tree(self,fileTree,prefix):
+    def __todict(self, fileTree):
+        fileTreeDict = dict()
         for file in fileTree:
             if file.isDirectory:
-                print(prefix + "[" + file.name + "]")
-                self.__print_file_tree(file.children, prefix + "  ")
+                fileTreeDict[file.name] = self.__todict(file.children)
             else:
-                print(prefix + file.name)
-        return
+                fileTreeDict[file.name] = ""
+        return fileTreeDict
+
+    def todict(self):
+        return self.__todict(self.fileTree)
+
+    def __file_tree_str(self, fileTree, prefix):
+        fileTreeStr = ""
+        for file in fileTree:
+            if file.isDirectory:
+                fileTreeStr = fileTreeStr + prefix + "[" + file.name + "]" + os.linesep
+                fileTreeStr = fileTreeStr + self.__file_tree_str(file.children, prefix + "  ")
+            else:
+                fileTreeStr = fileTreeStr + prefix + file.name + os.linesep
+        return fileTreeStr
     
-    def print_file_tree(self):
-        self.__print_file_tree(self.fileTree,"")
-        return
+    def __str__(self):
+        return self.__file_tree_str(self.fileTree,"").strip()
 
 
 def main():
-  parser = argparse.ArgumentParser(description='Parse file structure')
-  parser.add_argument("--path", "-p", help="Parent path", type=str, default=".")
+    parser = argparse.ArgumentParser(description='Parse file structure')
+    parser.add_argument("--path", "-p", help="Parent path", type=str, default=".")
   
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  files = FileTree(os.path.abspath(args.path))
+    files = FileTree(os.path.abspath(args.path))
 
-  files.print_file_tree()
+    print("File tree as string:")
+    print(files)
 
-  return 0
+    print("")
+    print("File tree as dict:")
+    pprint.pprint(files.todict())
+
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
